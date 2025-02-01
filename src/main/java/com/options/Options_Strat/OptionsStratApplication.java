37c6aac6276ec1cf4c867b4a9dcaf56703c84270
Inder Singh Chandel <inderchandel782@gmail.com>
2025-02-01 19:24:17 +0530

    first commit

---
+++ src/main/java/com/options/Options_Strat/OptionsStratApplication.java
@@ -0,0 +1,13 @@
+package com.options.Options_Strat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionsStratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionsStratApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/options/Options_Strat/OptionsStratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.options.Options_Strat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsStratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
